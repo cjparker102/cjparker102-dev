244c6e4c4d07550caafb72ac48223361eca06213
Christopher Parker <cjparker102@gmail.com>
2026-03-29 23:06:40 -0400

    fix favicon transparent background

--- app/icon.tsx
+++ app/icon.tsx
@@ -12,7 +12,7 @@ export default function Icon() {
       <div
         style={{
           fontSize: 18,
-          background: '#080C10',
+          background: 'transparent',
           width: '100%',
           height: '100%',
           display: 'flex',
