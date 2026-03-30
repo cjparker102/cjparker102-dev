f8f9d05c713b4477984199a3be3735c8f7e3b892
Christopher Parker <cjparker102@gmail.com>
2026-03-29 22:57:10 -0400

    add custom {C} favicon

---
+++ app/icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from 'next/og'
+
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 18,
+          background: '#080C10',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 700,
+        }}
+      >
+        <span style={{ color: '#E8A83E', marginRight: -2 }}>{'{'}</span>
+        <span style={{ color: '#FFFFFF', fontSize: 20 }}>C</span>
+        <span style={{ color: '#E8A83E', marginLeft: -2 }}>{'}'}</span>
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
